7b83e804983c67dda7b6405abb9f11eced18334c
bismeet <bismeetsingh352@gmail.com>
2018-10-04 19:03:52 +0530

    First commit

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <game :numberOfCards="12" :numberOfMatches="2"/>
+  </div>
+</template>
+
+<script>
+import Game from '@/components/TheGame';
+
+export default {
+  components: {
+    Game
+  }
+};
+</script>
+
+<style lang="stylus">
+@import 'common/theme'
+
+html, body
+  height 100%
+
+body 
+  margin 0
+  display flex
+  flex-direction column
+  color theme-black
+  font-family 'Avenir', Helvetica, Arial, sans-serif
+  -webkit-font-smoothing: antialiased
+  -moz-osx-font-smoothing: grayscale
+
+footer
+  text-align center
+  margin 10px 0
+  & .redheart
+    color theme-red
+
+#app 
+  flex 1 0 auto
+</style>
